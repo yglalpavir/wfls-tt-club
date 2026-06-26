1f045f645eeef65bd8ee33c7cd4512750582ff3e
yglalpavir <yglalpavir@gmail.com>
2026-06-27 02:33:24 +0800

    6-27

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
 async function loadRankingDataForViz() { try { await Promise.all([loadInitialScores(), loadEventCoefficients(), loadSeasons(), loadScoreLogForViz()]); if (!initialScoresData || !eventCoefficients || !seasonsData) throw new Error('数据加载失败'); rankingTimeline = calculateAllRankingsWithSeasons(scoreLogData, initialScoresData.initialScores, seasonsData); const rt = calculateRealtimeRanking(); if (rt) rankingTimeline.push(rt); return true; } catch(e) { console.error('DataViz: 排名计算失败', e); rankingTimeline = []; return false; } }
 
 function initPage() {
-    loadAboutData(); loadMembersData(); loadNewsData(); loadCompetitionsData(); loadQaData();
+    loadAboutData(); loadMembersData(); loadNewsData(); loadCompetitionsData(); loadQaData(); loadChangelogData();
     initCommon();
     const isRanking = !!document.getElementById('rankingFullBody'), isDataViz = !!document.getElementById('pointsTrendChart');
     if (isRanking) { loadRankingData(); }
